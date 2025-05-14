9b5aa49f321a816fc1dd4f4477fd1c35d521a84f
dapilot954 <eryk.gebski@hotmail.com>
2025-05-14 08:10:19 +0400

    test2 (temp)

--- Apps/FileExplorer.cs
+++ Apps/FileExplorer.cs
@@ -1,15 +1,13 @@
 ﻿using Cosmos.System.Graphics;
+using Cosmos.System;
 using PilotOS.Graphics;
 using PilotOS.Resources;
 using PilotOS.System;
 using System;
-using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PilotOS.Apps
 {
@@ -18,6 +16,10 @@ namespace PilotOS.Apps
         public static Bitmap Folder;
         public static Bitmap File;
         public string Path = @"0:\";
+        public static int scrollOffset = 0;
+        private const int itemHeight = 30;
+        private const int scrollSpeed = 10;
+
         public override void Run()
         {
             int x = WindowData.WinPos.X;
@@ -25,88 +27,90 @@ namespace PilotOS.Apps
             int SizeX = WindowData.WinPos.Width;
             int SizeY = WindowData.WinPos.Height;
 
-            var Directories = Directory.GetDirectories(Path);
-            List<string> Folders = new List<string>();
-
-            for (int i = 0; i < Directories.Length; i++)
-            {
-                Folders.Add(Directories[i]);
-            }
-
-            List<string> Files = new List<string>();
-            var File_raw = Directory.GetFiles(Path);
-
-            for (int i = 0; i < File_raw.Length; i++)
-            {
-                Files.Add(File_raw[i]);
-            }
-
+            var Directories = Directory.GetDirectories(Path).ToList();
+            var Files = Directory.GetFiles(Path).ToList();
 
             Window.DrawTop(this);
             GUI.MainCanvas.DrawFilledRectangle(GUI.colors.ColorMain, x, y + Window.TopSize, SizeX, SizeY - Window.TopSize);
             GUI.MainCanvas.DrawFilledRectangle(GUI.colors.ColorText, x, y + Window.TopSize, SizeX, 30);
             GUI.MainCanvas.DrawFilledRectangle(GUI.colors.ColorExplorer, x + 1, y + Window.TopSize + 1, SizeX - 2, 28);
+
             string Disp_path = TrimStringFromLeft(Path, SizeX - 30);
             GUI.MainCanvas.DrawString(Disp_path, GUI.FontDefault, GUI.colors.ColorText, x + 2, y + Window.TopSize + 7);
-            Positions(x, y + 60, SizeX, SizeY - 60, Folders , Files, 0);
-            
 
+            HandleScrollWheel();
+
+            DrawList(x, y + Window.TopSize + 35, SizeX, SizeY - Window.TopSize - 35, Directories, Files);
         }
 
-        public static void Positions(int startX, int startY, int width, int height, List<string> folders, List<string> files, int pageNo)
+        private void HandleScrollWheel()
         {
-            const int iconWidth = 70;
-            const int iconHeight = 86;
-
-            int itemsPerRow = width / iconWidth;
-            if (itemsPerRow <= 0) itemsPerRow = 1;
-
-            int itemsPerCol = height / iconHeight;
-            if (itemsPerCol <= 0) itemsPerCol = 1;
-
-            int itemsPerPage = itemsPerRow * itemsPerCol;
-
-            // Combine folders and files into one list with folder priority
-            var allItems = folders.Select(f => (f, true))   // true = isFolder
-                           .Concat(files.Select(f => (f, false)))
-                           .ToList();
-
-            int startIndex = pageNo * itemsPerPage;
-            int endIndex = Math.Min(startIndex + itemsPerPage, allItems.Count);
-
-            int indexOnPage = 0;
-
-            for (int i = startIndex; i < endIndex; i++)
+            int delta = MouseManager.ScrollDelta;
+            if (delta != 0)
             {
-                var (name, isFolder) = allItems[i];
-                int row = indexOnPage / itemsPerRow;
-                int col = indexOnPage % itemsPerRow;
-
-                int posX = startX + col * iconWidth;
-                int posY = startY + row * iconHeight;
-
-                if (isFolder)
-                    DrawFolder(posX, posY, name);
-                else
-                { DrawFile(posX, posY, name); }
-                   
-
-                indexOnPage++;
+                scrollOffset -= delta * scrollSpeed;
+                if (scrollOffset < 0) scrollOffset = 0;
+                // Add a max scrollOffset clamp if needed.
+                MouseManager.ResetScrollDelta();
             }
         }
 
-        public static void DrawFolder(int x, int y, string name)
+        private void DrawList(int startX, int startY, int width, int height, List<string> folders, List<string> files)
         {
-            GUI.MainCanvas.DrawImageAlpha(Folder, x+10, y+15);
-            GUI.MainCanvas.DrawString(TrimStringFromRight(name, 70),GUI.FontDefault, GUI.colors.ColorText, x, y + 70);
+            var allItems = folders.Select(f => (f, true)).Concat(files.Select(f => (f, false))).ToList();
 
+            int maxVisibleItems = height / itemHeight;
+            int totalHeight = allItems.Count * itemHeight;
+            int maxOffset = Math.Max(0, totalHeight - height);
+            if (scrollOffset > maxOffset) scrollOffset = maxOffset;
+
+            int yOffset = -scrollOffset;
+
+            for (int i = 0; i < allItems.Count; i++)
+            {
+                int itemY = startY + (i * itemHeight) + yOffset;
+                if (itemY + itemHeight < startY || itemY > startY + height)
+                    continue; // Skip drawing outside visible area
+
+                var (name, isFolder) = allItems[i];
+
+                string itemName = name.Substring(name.LastIndexOf('\\') + 1);
+
+                // Icon position (left-aligned)
+                int iconX = startX + 4;
+                int iconY = itemY + (itemHeight - 25) / 2;
+
+                // Text position (to the right of icon)
+                int textX = iconX + 25 + 5;
+                int textY = itemY + (itemHeight - 16) / 2; // roughly center vertically
+
+                GUI.MainCanvas.DrawString(
+                    TrimStringFromRight(itemName, width - (textX - startX) - 5),
+                    GUI.FontDefault,
+                    GUI.colors.ColorText,
+                    textX,
+                    textY
+                );
+
+                if (isFolder)
+                    GUI.MainCanvas.DrawImageAlpha(Folder, iconX, iconY);
+                else
+                    GUI.MainCanvas.DrawImageAlpha(File, iconX, iconY);
+            }
+
+            DrawScrollBar(startX + width - 6, startY, 6, height, totalHeight);
         }
 
-        public static void DrawFile(int x, int y, string name)
-        {
-            GUI.MainCanvas.DrawImageAlpha(File, x + 10, y + 15);
-            GUI.MainCanvas.DrawString(TrimStringFromRight(name, 70), GUI.FontDefault, GUI.colors.ColorText, x, y + 70);
 
+        private void DrawScrollBar(int x, int y, int width, int height, int contentHeight)
+        {
+            if (contentHeight <= height) return;
+
+            int barHeight = Math.Max((height * height) / contentHeight, 20);
+            int barY = y + (scrollOffset * height) / contentHeight;
+
+            GUI.MainCanvas.DrawFilledRectangle(GUI.colors.ColorText, x, y, width, height);
+            GUI.MainCanvas.DrawFilledRectangle(GUI.colors.ColorExplorer, x, barY, width, barHeight);
         }
 
         public static string TrimStringFromLeft(string input, int maxPixels)
@@ -121,11 +125,9 @@ namespace PilotOS.Apps
             if (input.Length * charWidth <= maxPixels)
                 return input;
 
-            // Reserve space for "..."
             int visibleChars = maxChars - ellipsis.Length;
-
             if (visibleChars <= 0)
-                return ellipsis.Substring(0, maxChars); // Not enough room for any characters
+                return ellipsis.Substring(0, maxChars);
 
             string trimmed = input.Substring(input.Length - visibleChars);
             return ellipsis + trimmed;
@@ -143,17 +145,12 @@ namespace PilotOS.Apps
             if (input.Length * charWidth <= maxPixels)
                 return input;
 
-            // Reserve space for "..."
             int visibleChars = maxChars - ellipsis.Length;
-
             if (visibleChars <= 0)
-                return ellipsis.Substring(0, maxChars); // Not enough room for any characters
+                return ellipsis.Substring(0, maxChars);
 
             string trimmed = input.Substring(0, visibleChars);
             return trimmed + ellipsis;
         }
-
-
     }
-
 }
